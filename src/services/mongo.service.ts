b8ce3f9c67ff15a0932d40bd6ee091f40d3b2d15
PAFFx <jj2jom@gmail.com>
2024-02-17 19:23:49 +0700

    add mongo.service containing DB class , db entity and connectToDatabase func

--- src/services/mongo.service.ts
+++ src/services/mongo.service.ts
@@ -1,1 +1,24 @@
+import * as mongo from 'mongodb';
+import config from '../configs/config';
+import dbConfig from '../configs/db.config';
 
+const connectionString = config.STATUS == 'production' ? config.PROD_DB_URL : config.TEST_DB_URL;
+
+class DB extends mongo.Db {
+  constructor(client: mongo.MongoClient) {
+    super(client, dbConfig.dbName);
+    this.boatsColl = this.collection(dbConfig.boatsColl);
+    this.emergenciesColl = this.collection(dbConfig.emergenciesColl);
+  }
+  boatsColl: mongo.Collection;
+  emergenciesColl: mongo.Collection;
+}
+
+export let db: DB;
+
+export async function connectToDatabase() {
+  const client: mongo.MongoClient = new mongo.MongoClient(connectionString);
+  await client.connect();
+
+  db = new DB(client);
+}
