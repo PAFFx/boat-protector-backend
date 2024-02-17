93132ab29f0d42f201d30efd169c723f08a458c5
PAFFx <jj2jom@gmail.com>
2024-02-17 19:23:03 +0700

    add config.ts for env and db.config.ts for dbConfig

--- src/configs/db.config.ts
+++ src/configs/db.config.ts
@@ -1,1 +1,13 @@
-// db configuration
+interface dbConfig {
+  dbName: string;
+  boatsColl: string;
+  emergenciesColl: string;
+}
+
+const dbConfig = {
+  dbName: 'boatProtector',
+  boatsColl: 'boats',
+  emergenciesColl: 'emergencies',
+};
+
+export default dbConfig;
