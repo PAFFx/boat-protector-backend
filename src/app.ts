4a4134d68a9de2494bab7a44929e58dc07514a00
PAFFx <jj2jom@gmail.com>
2024-02-17 19:23:56 +0700

    implement database connection in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,22 @@
 import express, { Express } from 'express';
 
 import { boatRouter } from './routes/boat.route';
+import config from './configs/config';
+import { connectToDatabase } from './services/mongo.service';
 
 const app: Express = express();
 
-// add router to app
-app.use('/ships', boatRouter);
+console.log(`launch in ${config.STATUS} mode`);
+const port: number = config.STATUS == 'production' ? config.PROD_PORT! : config.TEST_PORT;
 
-const port: number = 3000;
-app.listen(port, () => console.log(`Application is running on port ${port}`));
+connectToDatabase()
+  .then(() => {
+    // add router
+    app.use('/boats', boatRouter);
+    // run server
+    app.listen(port, () => console.log(`Application is running on port ${port}\n`));
+  })
+  .catch((e: Error) => {
+    console.error('Database connection failed', e);
+    process.exit();
+  });
