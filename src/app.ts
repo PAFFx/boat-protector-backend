476c26599af242a295b13fd43a3468e9da44bc38
PAFFx <jj2jom@gmail.com>
2024-02-29 15:34:52 +0700

    add cors

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,14 @@ import express, { Express } from 'express';
 import { boatRouter } from './routes/boat.route';
 import config from './configs/config';
 import { connectToDatabase, disconnectFromDatabase } from './services/mongo.service';
+var cors = require('cors');
 
 const app: Express = express();
 
 console.log(`launch in ${config.STATUS} mode`);
 const port: number = config.STATUS == 'production' ? config.PROD_PORT! : config.TEST_PORT;
 
+app.use(cors());
 app.use(express.json());
 app.use(
     express.urlencoded({
